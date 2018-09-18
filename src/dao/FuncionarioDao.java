c60a697cb1f45eea2f1052e179bddb17cf1a331e
aluno <aluno@LAB03MC08>
2018-09-18 17:25:39 -0300

    PROJETO 2018

--- src/dao/FuncionarioDao.java
+++ src/dao/FuncionarioDao.java
@@ -2,6 +2,8 @@
 package dao;
 import banco.Conexao;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import modelo.Funcionario;
 
 
@@ -12,8 +14,30 @@ public class FuncionarioDao {
         Funcionario funcionario = null;
         
         try (Connection con = Conexao.get()){
+            String sql = "SELECT * FROM usuario" +"WHERE login=? and senha=?";
+            
+            PreparedStatement prep = con.prepareStatement(sql);
+            prep.setString(1,login);
+            prep.setString(2,senha);
+            
+            ResultSet result = prep.executeQuery();
+            
+            if (result.first()){
+                
+            funcionario = new Funcionario ();
+            
+            funcionario.setId(result.getInt("id"));
+            funcionario.setNome(result.getString("nome"));
+            funcionario.setCargo(result.getString("cargo"));
+            funcionario.setLogin(login);
+            funcionario.setSenha(senha);
+            return funcionario;
+            
+            }
+            
         
     } catch(Exception e){
+        e.printStackTrace();
         
     }
         return funcionario;
--- src/dao/ProdutoDao.java
+++ src/dao/ProdutoDao.java
@@ -5,10 +5,41 @@
  */
 package dao;
 
-/**
- *
- * @author aluno
- */
+import banco.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import modelo.Produto;
+
+
 public class ProdutoDao {
     
+    
+    
+    public boolean salvar(Produto produto) {
+
+        try (Connection con = Conexao.get();) {
+
+            String sql = "SELECT * FORM produto"+" WHERE (nome,descricao,precoCusto,porcentagemLucro,precoVenda,quantidadeEstoque,unidade) values(?,?,?,?,?,?,?)";
+            PreparedStatement prep = con.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setString(2, produto.getDescricao());
+            prep.setDouble(3,produto.getPrecoCusto());
+            prep.setDouble(4,produto.getPorcentagemLucro());
+            prep.setDouble(5,produto.getPrecoVenda());
+            prep.setDouble(6,produto.getQuantidadeEstoque());
+            prep.setString(7,produto.getUnidade());
+            
+          
+
+            return true;
+
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+            return false;
+        }
+
+    }
 }
+    
+    
